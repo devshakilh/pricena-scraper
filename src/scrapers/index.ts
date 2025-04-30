fe7bff6a69c0cbd6d4a7a8a81ebfe8424fc63770
Md Shakil Hossain <shakilshifat65@gmail.com>
2025-04-30 21:30:11 +0600

    Fix TypeScript output path and clean JS files

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -14,42 +14,42 @@ import { scraperSources } from '../config/scraper.config';
 export const scrapers: Record<string, Scraper> = {
   daraz: new DarazScraper(
     scraperSources.find((s) => s.key === 'daraz')!.baseUrl,
-    scraperSources.find((s) => s.key === 'daraz')!.domain,
+    scraperSources.find((s) => s.key === 'daraz')!.domain
   ),
   chaldal: new ChaldalScraper(
     scraperSources.find((s) => s.key === 'chaldal')!.baseUrl,
-    scraperSources.find((s) => s.key === 'chaldal')!.domain,
+    scraperSources.find((s) => s.key === 'chaldal')!.domain
   ),
   ajkerdeal: new AjkerDealScraper(
     scraperSources.find((s) => s.key === 'ajkerdeal')!.baseUrl,
-    scraperSources.find((s) => s.key === 'ajkerdeal')!.domain,
+    scraperSources.find((s) => s.key === 'ajkerdeal')!.domain
   ),
   pickaboo: new PickabooScraper(
     scraperSources.find((s) => s.key === 'pickaboo')!.baseUrl,
-    scraperSources.find((s) => s.key === 'pickaboo')!.domain,
+    scraperSources.find((s) => s.key === 'pickaboo')!.domain
   ),
   othoba: new OthobaScraper(
     scraperSources.find((s) => s.key === 'othoba')!.baseUrl,
-    scraperSources.find((s) => s.key === 'othoba')!.domain,
+    scraperSources.find((s) => s.key === 'othoba')!.domain
   ),
   bagdoom: new BagdoomScraper(
     scraperSources.find((s) => s.key === 'bagdoom')!.baseUrl,
-    scraperSources.find((s) => s.key === 'bagdoom')!.domain,
+    scraperSources.find((s) => s.key === 'bagdoom')!.domain
   ),
   priyoshop: new PriyoShopScraper(
     scraperSources.find((s) => s.key === 'priyoshop')!.baseUrl,
-    scraperSources.find((s) => s.key === 'priyoshop')!.domain,
+    scraperSources.find((s) => s.key === 'priyoshop')!.domain
   ),
   shodagor: new ShodagorScraper(
     scraperSources.find((s) => s.key === 'shodagor')!.baseUrl,
-    scraperSources.find((s) => s.key === 'shodagor')!.domain,
+    scraperSources.find((s) => s.key === 'shodagor')!.domain
   ),
   rokomari: new RokomariScraper(
     scraperSources.find((s) => s.key === 'rokomari')!.baseUrl,
-    scraperSources.find((s) => s.key === 'rokomari')!.domain,
+    scraperSources.find((s) => s.key === 'rokomari')!.domain
   ),
   wafilife: new WafilifeScraper(
     scraperSources.find((s) => s.key === 'wafilife')!.baseUrl,
-    scraperSources.find((s) => s.key === 'wafilife')!.domain,
+    scraperSources.find((s) => s.key === 'wafilife')!.domain
   ),
 };
